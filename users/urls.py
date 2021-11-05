e923772843dd49b061db6666a550e089d34eca0f
tahirs95 <tahirs95@hotmail.com>
2021-11-05 17:16:28 +0500

    Endpoints for user auth, registration.

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from users.views import (
     user_redirect_view,
     user_update_view,
     user_detail_view,
+    register,
+    change_password,
+    logout
 )
 
 app_name = "users"
@@ -11,4 +15,9 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
+    path('rest-auth/login/', obtain_auth_token, name='api_token_auth'),
+    path('rest-auth/registration/', register, name='register'),
+    path('rest-auth/password/change/', change_password, name='change_password'),
+    path('rest-auth/logout/', logout, name='logout'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.views.generic import DetailView, RedirectView, UpdateView
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
 User = get_user_model()
 
 
@@ -40,3 +45,48 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 
 user_redirect_view = UserRedirectView.as_view()
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    request.user.auth_token.delete()
+    return Response({"OK", "Auth Token is deleted."}, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+def register(request):
+    name = request.POST.get("name")
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    email_check = User.objects.filter(email=email)
+    if email_check:
+        return Response(
+            {"Error": "Email already exists."}, status=status.HTTP_400_BAD_REQUEST
+        )
+    user = User.objects.create(
+        username=email, email=email, name=name
+    )
+    user.set_password(password)
+    user.save()
+    return Response({
+        'name': user.name,
+        'email': user.email,
+    }, status=status.HTTP_201_CREATED)
+
+@api_view(["POST"])
+def change_password(request):
+    email = request.POST.get("email")
+    password1 = request.POST.get("password1")
+    password2 = request.POST.get("password2")
+    if password1 != password2:
+        return Response(
+            {"error": "password not matched"}, status=status.HTTP_400_BAD_REQUEST
+        )
+    user = User.objects.filter(email=email)
+    if user:
+        user.set_password(password1)
+        user.save()
+        return Response({"OK": "password is reset."}, status=status.HTTP_201_CREATED)
+    else:
+        return Response(
+            {"Error": "User doesn't exist."}, status=status.HTTP_400_BAD_REQUEST
+        )
