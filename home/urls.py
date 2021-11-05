f8f4599f590ac474f80cf5cfc4ab86e86c453b44
tahirs95 <tahirs95@hotmail.com>
2021-11-05 17:16:05 +0500

    CRUD functionality for User Apps.

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import home
+from home import views
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", views.home, name="home"),
+    path("list/", views.ListUserAppAPIView.as_view(), name="list_apps"),
+    path("create/", views.CreateUserAppAPIView.as_view(), name="create_app"),
+    path("update/<int:pk>/", views.UpdateUserAppAPIView.as_view(), name="update_app"),
+    path("delete/<int:pk>/",views.DeleteUserAppAPIView.as_view(), name="delete_app"),
+    path("sub_retrieve/<int:pk>/", views.RetrieveSubscriptionAPIView.as_view(), name="retrieve_sub"),
+    path("update_sub/<int:pk>/", views.UpdateSubscriptionAPIView.as_view(), name="update_sub"),
+    path("associate_plan/", views.associate_plan, name="associate_plan"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,72 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
 
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import DestroyAPIView
+from rest_framework.generics import UpdateAPIView
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.decorators import api_view
+
+from .serializers import SubscriptionSerializer, UserAppSerializer
+from .models import UserApp, Subscription, Plan
+class ListUserAppAPIView(ListAPIView):
+    queryset = UserApp.objects.all()
+    serializer_class = UserAppSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return UserApp.objects.filter(user=user)
+
+class CreateUserAppAPIView(CreateAPIView):
+    serializer_class = UserAppSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        return UserApp.objects.filter(user=user)
+
+class UpdateUserAppAPIView(UpdateAPIView):
+    serializer_class = UserAppSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return UserApp.objects.filter(user=user)
+
+class DeleteUserAppAPIView(DestroyAPIView):
+    serializer_class = UserAppSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return UserApp.objects.filter(user=user)
+
+class RetrieveSubscriptionAPIView(RetrieveAPIView):
+    serializer_class = SubscriptionSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Subscription.objects.filter(app__user=user)
+class UpdateSubscriptionAPIView(UpdateAPIView):
+    serializer_class = SubscriptionSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Subscription.objects.filter(app__user=user)
+
+@api_view(['POST'])
+def associate_plan(request, pk):
+    sub_id = request.GET.get('id')
+    plan_id = request.POST.get('plan_id')
+    if sub_id and plan_id:
+        subscription = Subscription.objects.filter(id=sub_id)
+        plan = Plan.objects.filter(id=plan_id).first()
+        subscription.plan = plan
+        subscription.save()
+        return JsonResponse({'message': 'Plan associated.'})
+    return JsonResponse({'message': 'Plan not associated'}, status=status.HTTP_404_NOT_FOUND) 
 
 def home(request):
     packages = [
